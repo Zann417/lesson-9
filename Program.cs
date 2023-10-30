6c7213dd6f764d487cdd49dbeb72b436786b4e07
covalew.valery <covalew.valery@DESKTOP-D2P4U86>
2023-10-30 10:49:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите URL сайта: ");
+        string url = Console.ReadLine();
+
+        try
+        {
+            using (WebClient client = new WebClient())
+            {
+                string html = client.DownloadString(url);
+
+                string directoryPath = @"D:\Scenario";
+                string filePath = System.IO.Path.Combine(directoryPath, "page.html");
+                System.IO.Directory.CreateDirectory(directoryPath);
+                System.IO.File.WriteAllText(filePath, html);
+
+                Console.WriteLine("Страница успешно загружена и сохранена в файле " + filePath);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
